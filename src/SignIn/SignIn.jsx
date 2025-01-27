1db0690c7aa77b93e533ee7ac6fa4f4015cd605b
Gengo-bit <paulcorsino28@gmail.com>
2025-01-27 22:45:35 +0800

    try fix auth

--- src/SignIn/SignIn.jsx
+++ src/SignIn/SignIn.jsx
@@ -2,14 +2,21 @@ import React from 'react'
 import { auth, firebase } from '../FirebaseConfig/FirebaseConfig';
 
 function SignIn() {
-const signInWithGoogle = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider);
-    }
     
+    const signInWithGoogle = async () => {
+      try {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        await auth.signInWithPopup(provider);
+        console.log("User signed in successfully");
+      } catch (error) {
+        console.error("Error during sign-in:", error.message);
+      }
+    };
+  
     return (
-    <button onClick={signInWithGoogle}>Sign in with Google</button>
-    )
+      <button onClick={signInWithGoogle}>Sign in with Google</button>
+    );
 }
+  
 
-export default SignIn
+export default SignIn;

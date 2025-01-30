20c8b1a3e10efc23407a034632be6ef3b7d5c3e5
Gengo-bit <paulcorsino28@gmail.com>
2025-01-30 23:12:02 +0800

    add basename to router

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div className="App">
-      <Router>
+      <Router basename="/">
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/ChatRoom" element={<ChatRoom />} />
